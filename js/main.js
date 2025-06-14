9d0a527cb7d788805ecc8e82faed87777c373dd0
Ryan Meneses <ryanmeneses.dev@gmail.com>
2025-06-14 15:20:03 -0300

    Proteção dados API

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { firebaseConf } from "./firebaseConfig.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfsAqNVdA2YJGGiHYGOk_lKTgc9fO--aM",
-  authDomain: "projeto-ifsp-3-semestre.firebaseapp.com",
-  projectId: "projeto-ifsp-3-semestre",
-  storageBucket: "projeto-ifsp-3-semestre.firebasestorage.app",
-  messagingSenderId: "723939427008",
-  appId: "1:723939427008:web:0ce661dc92adb7598be325"
+  apiKey: firebaseConf.apiKey,
+  authDomain: firebaseConf.authDomain,
+  projectId: firebaseConf.projectId,
+  storageBucket: firebaseConf.storageBucket,
+  messagingSenderId: firebaseConf.messagingSenderId,
+  appId: firebaseConf.appId
 };
 
 const app = initializeApp(firebaseConfig);
